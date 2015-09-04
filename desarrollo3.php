a8c270804ebda23fc3dabd1b5eea3955fa8f28d1
paola <lina.02paola@gmail.com>
2015-09-04 07:55:16 +0200

    ejercicio 3 terminado

---
+++ desarrollo3.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+   if (isset($_POST['area'])){
+            $tipo = $_POST['area'];
+            if ($tipo == "Circulo"){
+                $valor = $_POST['valor'];
+                $valor1 = $valor * $valor;                        
+                $b= 3.1416 * $valor1; 
+                echo "el area del circulo es:".$b;
+                
+            }elseif ($tipo = "Circunferencia") {
+                $valor = $_POST['valor'];
+                $valor1 = $valor + $valor;                        
+                $a= 3.1416 * $valor1;
+                
+                echo "la longitu de la circuferencia es:".$a;
+            }    
+        }
+
+
+?>
---
+++ ejercicio3.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+    <form action="desarrollo3.php" method="POST">
+        
+    <p>A03:  Por medio de un menú dar al usuario la opción de seleccionar círculo o circunferencia si selecciona círculo calcular su área. A=PI*r^2, si selecciona circunferencia calcular su longitud. </p> <br>    
+      
+        <form id="formularioFigura" name="formularioFigura" action="desarrollo3.php" method="POST">
+           <label><em>Seleccione el tipo de figura que desea conocer:</em>
+            <select id="area" name="area">
+                <option value="Circulo">Circulo</option>
+                <option value="Circunferencia">Circunferencia</option>
+            </select><br>
+            <br/>
+               
+                <input type="submit" value="Enviar">
+               
+         
+        
+    </body>
+</html>
+
+
+
+
+
+
+
